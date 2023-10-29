dbd8ade965f24c6372e59c43fa132fb57bf7ce2d
hliosone <sstelcher@protonmail.com>
2023-10-29 19:55:05 +0100

    File management

--- App.java
+++ App.java
@@ -1,16 +1,21 @@
 import java.io.BufferedReader;
 import java.util.*;
-import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader; //package for input reader
+import java.io.IOException;
 
+// Utilisation de l'API NIO
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.nio.charset.Charset;
+
+//importation des classes (package)
 import fr.sstelcher.*;
-
 
 public class App {
     public static void main(String[] args) {
-
-      
-
-
+     
     }
 }
